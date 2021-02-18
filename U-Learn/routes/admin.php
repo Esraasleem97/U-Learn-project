a7c3492b6f518a8b3b62acd5d770c076b4bb2c76
ahmadzreqat <ahmedzreqat1@gmail.com>
2021-02-18 19:08:46 +0200

    admin

--- U-Learn/routes/admin.php
+++ U-Learn/routes/admin.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'login'  , 'middleware' => 'guest'] , function() {
-    // it will be protected with admin guard soon ...
-Route::view('/register','backend.auth.register')->name('admin.register');
+
+Route::group(['prefix' => 'admin', 'middleware' => 'guest'], function () {
+
+
+        // it will be protected with admin guard soon ...
+        Route::view('/register', 'backend.auth.register')->name('admin.register');
+
+
 });
